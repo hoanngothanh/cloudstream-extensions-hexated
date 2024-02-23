79c1d70ac556c793609021518933ce1a5c9b8575
Ngô Thanh Hoàn <hoanngothanh@gmail.com>
2024-02-24 03:05:39 +0700

    update phimmoi url

--- PhimmoichillProvider/src/main/kotlin/com/hexated/PhimmoichillProvider.kt
+++ PhimmoichillProvider/src/main/kotlin/com/hexated/PhimmoichillProvider.kt
@@ -9,7 +9,7 @@ import java.net.URI
 import java.net.URLDecoder
 
 class PhimmoichillProvider : MainAPI() {
-    override var mainUrl = "https://phimmoichillk.net"
+    override var mainUrl = "https://phimmoichille.net"
     private var directUrl = mainUrl
     override var name = "Phimmoichill"
     override val hasMainPage = true
